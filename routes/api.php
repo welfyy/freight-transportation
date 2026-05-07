028bc28ebd2f3eeaf53f587524cd05d7d0419dda
welfyy <vanyakostritsia@gmail.com>
2026-05-07 22:02:04 +0300

    Фіналізація API: перехід на Production режим, виправлення помилок кешування та налаштування CORS

--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,12 @@ Route::get('/trips/{id}', [TripController::class, 'show']);      // Один р�
 Route::post('/trips/store', [TripController::class, 'store']);  // Додавання
 Route::put('/trips/{id}', [TripController::class, 'update']);    // Редагування
 Route::delete('/trips/{id}', [TripController::class, 'destroy']); // Видалення
+
+// Додаємо маршрут для отримання категорій (типів рейсів або вантажів)
+Route::get('/categories', function () {
+    return [
+        ['id' => 1, 'name' => 'Міжнародні'],
+        ['id' => 2, 'name' => 'Внутрішні'],
+        ['id' => 3, 'name' => 'Експрес'],
+    ];
+});
